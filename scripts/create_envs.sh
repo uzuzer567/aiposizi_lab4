2df29301a61740ab247e56d7fa7474617d952f8d
uzuzer567 <49733302+uzuzer567@users.noreply.github.com>
2021-05-20 10:39:33 +0300

    feat: add create_envs file

---
+++ scripts/create_envs.sh
@@ -0,0 +1,7 @@
+export DATABASE_HOST=0.0.0.0
+export DATABASE_PORT=5432
+export DATABASE_USER=vadbeg
+export DATABASE_PASSWORD=password
+export DATABASE_NAME=database
+export API_PREFIX=/api/v_0
+export ROOT_BACKEND_URI=http://0.0.0.0:9000/api/v_0/
